8abb13ed18a42cee35fe4d57301d2f682c9dae6e
Ryan Lunger <ryanlunger@gmail.com>
2020-04-18 19:27:20 -0500

    Implements basic password generator.
    Features copy-to-clipboard and some basic functional programming.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,89 @@
 import { Component } from '@angular/core';
+import { isNullOrUndefined } from 'util';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'modern-angular-bootcamp-password-generator';
+
+  ///////////////////////////////////
+  // Private Members
+  ///////////////////////////////////
+  private useLetters = 'Use Letters';
+  private useNumbers = 'Use Numbers';
+  private useSymbols = 'Use Symbols';
+
+  ///////////////////////////////////
+  // Bound Members
+  ///////////////////////////////////
+  password = '';
+  passwordLength = 8;
+
+  options = {
+    [this.useLetters]: true,
+    [this.useNumbers]: true,
+    [this.useSymbols]: true,
+  };
+
+  ////////////////////////////////////
+  // Data Contributors
+  ////////////////////////////////////
+  contributors = [
+    {
+      name: this.useLetters, method: () => AppComponent.pickRandom('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz')
+    },
+    {
+      name: this.useNumbers, method: () => AppComponent.pickRandom('0123456789')
+    },
+    {
+      name: this.useSymbols, method: () => AppComponent.pickRandom('!@#$%^*()')
+    }
+  ];
+
+  ////////////////////////////////////////////
+  // Static Methods
+  ////////////////////////////////////////////
+  static pickRandom = (charset: string) => charset.charAt(Math.floor(Math.random() * charset.length));
+
+  ////////////////////////////////////////////
+  // Instance Methods
+  ////////////////////////////////////////////
+  setOption(option: string) {
+    this.options[option] = !this.options[option];
+  }
+
+  onSubmit() {
+    const contributorReducer = (acc: any, curr: { method: () => string; }) => {
+      if (!acc || Math.random() > 0.5) {
+        return curr.method();
+      }
+
+      return acc;
+    };
+
+    const enabledContributors = this.contributors.filter(contributor => this.options[contributor.name]);
+
+    this.password = new Array(this.passwordLength)
+      .fill(undefined)
+      .map(each => enabledContributors.reduce(contributorReducer, each))
+      .join('');
+  }
+
+  onCopy() {
+    navigator.clipboard.writeText(this.getPassword());
+  }
+
+  getPassword() {
+    return this.password;
+  }
+
+  getClipboardClass() {
+    if (this.password === '') {
+      return 'hidden';
+    }
+  }
 }
